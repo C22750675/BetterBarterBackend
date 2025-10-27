581ecef88b57723bf43db603ef95494f259bbe3e
Hugo Garry <hugog@optty.com>
2025-10-27 17:31:06 +0000

    Add configuration and database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ItemsModule } from './items/items.module';
+import { CirclesModule } from './circles/circles.module';
+import { TradesModule } from './trades/trades.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available everywhere
+      envFilePath: '.env', // Specifies the .env file to load
+    }),
+
+    // Setup TypeORM with PostgreSQL using ConfigService
+    TypeOrmModule.forRootAsync({
+      // We are importing ConfigModule here to make ConfigService available for injection
+      imports: [ConfigModule],
+      // The useFactory function will be called to create the configuration object
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // synchronize: true should NOT be used in production.
+        // It's useful for development as it auto-creates the DB schema.
+        synchronize: true,
+      }),
+      // This tells NestJS to inject the ConfigService into our useFactory function
+      inject: [ConfigService],
+    }),
+
+    // Feature modules
+    UsersModule,
+    ItemsModule,
+    CirclesModule,
+    TradesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
