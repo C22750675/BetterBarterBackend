2db2b112e338bf4963740bb31b63e9d766ec3cd8
Hugo Garry <hugog@optty.com>
2025-11-04 15:14:18 +0000

    Enhance RegisterUserDto validation with username and password strength checks

--- src/users/dtos/register-user.dto.ts
+++ src/users/dtos/register-user.dto.ts
@@ -1,12 +1,22 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^\S+$/, {
+    message: 'Username cannot contain spaces.',
+  })
   username: string;
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+      message:
+        'Password is too weak. It must contain an uppercase letter, a lowercase letter, a number, and a special character.',
+    },
+  )
   password: string;
 }
