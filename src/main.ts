759ec54158048862688b1fd2cd5a9bfd79ac0969
Hugo Garry <hugog@optty.com>
2025-11-23 16:15:37 +0000

    Add Swagger documentation setup for BetterBarter API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -19,6 +20,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('BetterBarter API')
+    .setDescription('The BetterBarter API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   // Add the '0.0.0.0' host for emulator connectivity
   await app.listen(3000, '0.0.0.0');
