c26f038f4b19af44cb06218df86d4270e7ac2bc5
Hugo Garry <hugog@optty.com>
2026-03-01 22:24:02 +0000

    Improve naming of the rep config file.

--- src/config/reputation.config.ts
+++ src/config/reputation.config.ts
@@ -1,4 +1,4 @@
-export default () => ({
+const reputation = () => ({
   reputation: {
     weights: {
       history: 0.55,
@@ -18,3 +18,4 @@ export default () => ({
     },
   },
 });
+export default reputation;
