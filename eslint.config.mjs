dd1a5f391f6d04598a13cdabdfe1d74c2c226265
Hugo Garry <hugog@optty.com>
2026-03-01 15:21:09 +0000

    Update ESLint configuration to set source type to module

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig([
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
