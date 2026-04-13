b6f47be500cf3579399a265bf8a50077cdb4db1b
Hugo Garry <hugog@optty.com>
2026-04-13 22:07:55 +0100

    Update import statement for 'join' to use 'node:path' for consistency

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import { join } from 'node:path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
